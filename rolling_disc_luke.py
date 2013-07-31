43eeff850700935cd386d465df0e1ccc3818c4d1
Dale Lukas Peterson <hazelnusse@gmail.com>
2013-07-31 16:08:26 -0700

    Nearly finished example.

--- rolling_disc_luke.py
+++ rolling_disc_luke.py
@@ -44,12 +44,10 @@ B = A.orientnew('B', 'Axis', [q2, A.x])   # Lean intermediate frame
 C = B.orientnew('C', 'Axis', [q3, B.y])   # Disc fixed frame
 # Disc angular velocity in N expressed using time derivatives of coordinates
 w_c_n_qd = C.ang_vel_in(N)
+w_b_n_qd = B.ang_vel_in(N)
 
 # Inertial angular velocity and angular acceleration of disc fixed frame
-C.set_ang_vel(N, u1*C.x + u2*C.y + u3*C.z)
-C.set_ang_acc(N, u1d*C.x + u2d*C.y + u3d*C.z)
-print("Angular acceleration")
-mprint(C.ang_acc_in(N))
+C.set_ang_vel(N, u1*B.x + u2*B.y + u3*B.z)
 
 # Points
 NO = Point('NO')                                       # Inertial origin
@@ -58,48 +56,35 @@ CO = NO.locatenew('CO', q4*N.x + q5*N.y + q6*N.z)      # Disc center
 v_co_n_qd = CO.pos_from(NO).dt(N)
 
 CO.set_vel(N, u4*C.x + u5*C.y + u6*C.z)
-print("Translational acceleration")
-mprint(CO.acc(N))
 
 P = CO.locatenew('P', r*B.z)                           # Disc-ground contact
 P.v2pt_theory(CO, N, C)
-P.a2pt_theory(CO, N, C)
 
 # Configuration constraint and its Jacobian w.r.t. q        (Table 1)
 f_c = Matrix([q6 - dot(CO.pos_from(P), N.z)])
 f_c_dq = f_c.jacobian(q)
-print("Configuration constraint")
-mprint(f_c)
 
 # Velocity level constraints                                (Table 1)
 f_v = Matrix([dot(P.vel(N), uv) for uv in C])
 f_v_dq = f_v.jacobian(q)
 f_v_du = f_v.jacobian(u)
-print("Velocity constraints")
-mprint(f_v)
 
 # Kinematic differential equations
 kindiffs = Matrix([dot(w_c_n_qd - C.ang_vel_in(N), uv) for uv in B] +
-                  [dot(v_co_n_qd - CO.vel(N), uv) for uv in B])
+                  [dot(v_co_n_qd - cross(C.ang_vel_in(N), CO.pos_from(P)), uv) for uv in N])
+qdots = solve(kindiffs, qd)
 
-print("Kinematic differential equations")
-for kd_i in kindiffs:
-    print("{0} &= 0 \\\\".format(msprint(kd_i)))
+B.set_ang_vel(N, w_b_n_qd.subs(qdots))
+C.set_ang_acc(N, C.ang_vel_in(N).dt(B) + cross(B.ang_vel_in(N), C.ang_vel_in(N)))
 
 # f_0 and f_1                                               (Table 1)
 f_0 = kindiffs.subs(u_zero)
 f_1 = kindiffs.subs(qd_zero)
 
-# f_0 == f_0_coef_matrix * qd, used to solve for qdots
-f_0_coef_matrix = f_0.jacobian(qd)
-
 # Acceleration level constraints                            (Table 1)
-#v_co_n = cross(C.ang_vel_in(N), CO.pos_from(P))
-#a_co_n = v_co_n.dt(B) + cross(B.ang_vel_in(N), v_co_n)
-#f_a = Matrix([((a_co_n - CO.acc(N)) & uv).expand() for uv in B])
-f_a = f_v.diff(t)
-print("Acceleration level constraints")
-mprint(f_a)
+v_co_n = cross(C.ang_vel_in(N), CO.pos_from(P))
+a_co_n = v_co_n.dt(B) + cross(B.ang_vel_in(N), v_co_n)
+f_a = Matrix([((a_co_n - CO.acc(N)) & uv).expand() for uv in B])
 
 # Kane's dynamic equations via elbow grease
 # Partial angular velocities and velocities
@@ -110,8 +95,6 @@ partial_v_CO = [CO.vel(N).diff(ui, N) for ui in u]
 F_CO = m*g*A.z
 # Generalized active forces (unconstrained)
 gaf = [dot(F_CO, pv) for pv in partial_v_CO]
-print("Generalized active forces (unconstrained)")
-mprint(gaf)
 
 # Inertia force
 R_star_CO = -m*CO.acc(N)
@@ -163,12 +146,6 @@ gaf_dep = Matrix([gaf[i] for i in dep_indices])
 gif_con = gif_indep + Bd_inv_Bi.T * gif_dep
 gaf_con = gaf_indep + Bd_inv_Bi.T * gaf_dep
 
-print("Generalized inertia forces (constrained)")
-mprint(gif_con)
-
-print("Generalized active forces (constrained)")
-mprint(gaf_con)
-
 # Build the part of f_2 and f_3 that come from Kane's equations, the first three
 # rows of each
 f_2 = gif_con.subs(ud_sym).subs(u_zero).subs(qd_zero).subs(ud_sym_inv)
@@ -189,23 +166,12 @@ mprint(f_2)
 print("f_3:")
 mprint(f_3)
 
-# Linearization Code
-
-#qdep = Pqd.T * Matrix(q)
-#udep = Pud.T * Matrix(u)
-
-#n = len(q)
-#l = len(qdep)
-#o = len(u)
-#m = len(udep)
-
-# Point of linearization
-# equilibrium conditions:
+# Linearization Code solving, for equilibrium conditions:
 # Non-zero variables: q2, q1d, q3d, q4d, q5d
-eq_q = {q1: 0,          # yaw angle (ignorable)
-        q3: 0,          # spin angle (ignorable)
-        q4: 0,          # x of disc center (ignorable)
-        q5: 0,          # y of disc center (ignorable)
+eq_q = {#q1: 0,          # yaw angle (ignorable)
+#        q3: 0,          # spin angle (ignorable)
+#        q4: 0,          # x of disc center (ignorable)
+#        q5: 0,          # y of disc center (ignorable)
         q6: -r*cos(q2)} # z of disc center
 eq_qd = {q2d: 0}        # lean rate
 
@@ -225,24 +191,6 @@ for ui in [u4, u5, u6]:
 for qdi in [q4d, q5d, q6d]:
     eq_qd[qdi] = soln[qdi]
 
-print("Equilibrium coordinates:")
-for qi in q:
-    if qi in eq_q.keys():
-        s = msprint(qi) + " = " + msprint(eq_q[qi])
-        print(s)
-
-print("Equilibrium coordinate time derivatives:")
-for qdi in qd:
-    if qdi in eq_qd.keys():
-        s = msprint(qdi) + " = " + msprint(eq_qd[qdi])
-        print(s)
-
-print("Equilibrium generalized speeds:")
-for ui in u:
-    if ui in eq_u.keys():
-        s = msprint(ui) + " = " + msprint(eq_u[ui])
-        print(s)
-
 # Solve differentiated acceleration constraints for dependent du/dt's
 f_a_eq = f_a.subs(eq_qd).subs(ud_sym).subs(eq_u).subs(qd_sym).subs(eq_q).subs(qd_sym_inv).subs(ud_sym_inv)
 eq_ud = solve(f_a_eq, [u4d, u5d, u6d])
@@ -250,7 +198,6 @@ eq_ud = solve(f_a_eq, [u4d, u5d, u6d])
 # Evaluate the dynamic equations using the dependent du/dt's
 dyndiffs_eq = (f_2 + f_3).subs(eq_ud).subs(eq_qd).subs(ud_sym).subs(eq_u).subs(qd_sym).subs(eq_q).subs(qd_sym_inv).subs(ud_sym_inv).expand()
 
-mprint(dyndiffs_eq)
 # Solve the dynamic equations for the remaining two independent du/dt's
 soln = solve(dyndiffs_eq, [u1d, u2d, u3d])
 
@@ -261,41 +208,19 @@ for udi in [u4d, u5d, u6d]:
 for udi in [u1d, u2d, u3d]:
     eq_ud[udi] = soln[udi]
 
-print("Equilibrium generalized speed time derivatives:")
-for udi in ud:
-    if udi in eq_ud.keys():
-        s = msprint(udi) + " = " + msprint(eq_ud[udi])
-        print(s)
-
 print("f_c, f_v, f_a, f_0 + f_1, f_2 + f_3, evaluated at equilibrium conditions")
 f_c_eq = f_c.subs(eq_q)
 f_v_eq = f_v.subs(eq_u).subs(qd_sym).subs(eq_q).subs(qd_sym_inv)
 f_a_eq = f_a.subs(eq_ud).subs(eq_u).subs(qd_sym).subs(eq_q).subs(qd_sym_inv).expand()
+f_a_eq.simplify()
 f_0_eq_plus_f_1_eq = (f_0 + f_1).subs(eq_qd).subs(eq_u).subs(qd_sym).subs(eq_q).subs(qd_sym_inv)
 f_2_eq_plus_f_3_eq = (f_2 + f_3).subs(eq_ud).subs(eq_u).subs(qd_sym).subs(eq_q).subs(qd_sym_inv).expand()
 f_2_eq_plus_f_3_eq.simplify()
-mprint(f_c_eq)
-mprint(f_v_eq)
-mprint(f_a_eq)
-mprint(f_0_eq_plus_f_1_eq)
-mprint(f_2_eq_plus_f_3_eq)
-
-"""
-eom_eq = (gif_con + gaf_con).subs(ud_sym).subs(eq_qd).subs(eq_u).subs(qd_sym).subs(eq_q).subs(qd_sym_inv).subs(ud_sym_inv).expand()
-mprint(eom_eq)
-p = Poly(steady_turn_eqn, q1d)
-a, b, c = p.coeffs()
-discriminant = b*b - 4*a*c
-print("Steady turning balance equation:")
-print(msprint(steady_turn_eqn) + " = 0")
-print("Coefficients of quadratic in dq1/dt:")
-print("a = " + msprint(a))
-print("b = " + msprint(b))
-print("c = " + msprint(c))
-print("Discriminant which must be non-negative:")
-mprint(discriminant.simplify())
-stop
-"""
+assert(f_c_eq == zeros((1,1)))
+assert(f_v_eq == zeros((3,1)))
+assert(f_a_eq == zeros((3,1)))
+assert(f_0_eq_plus_f_1_eq == zeros((6,1)))
+assert(f_2_eq_plus_f_3_eq == zeros((3,1)))
 
 M_qq = f_0.jacobian(qd).subs(eq_ud).subs(eq_u).subs(eq_qd).subs(qd_sym).subs(eq_q).subs(qd_sym_inv)
 M_uqc = f_a.jacobian(qd).subs(eq_ud).subs(eq_u).subs(eq_qd).subs(qd_sym).subs(eq_q).subs(qd_sym_inv)
@@ -325,8 +250,6 @@ row3 = M_uqd.row_join(M_uud)
 M = row1.col_join(row2).col_join(row3)
 
 M.simplify()
-print("M:")
-mprint(M)
 
 row1 = ((A_qq + A_qu * C_1) * C_0).row_join(A_qu * C_2)
 row2 = ((A_uqc + A_uuc * C_1) * C_0).row_join(A_uuc * C_2)
@@ -334,20 +257,22 @@ row3 = ((A_uqd + A_uud * C_1) * C_0).row_join(A_uud * C_2)
 Amat = row1.col_join(row2).col_join(row3)
 
 Amat.simplify()
-print("A:")
-mprint(Amat)
 
 upright_nominal = {q1d: 0, q2: 0, m: 1, r: 1, g: 1}
 M_upright = M.subs(upright_nominal)
 A_upright = Amat.subs(upright_nominal)
 A_ss = M_upright.inv() * A_upright
-pprint(A_ss)
 perm = Pqi.row_join(zeros(6, 3)).col_join(zeros(6, 5).row_join(Pui))
 A_ss_red = perm.T * A_ss
-pprint(A_ss_red)
 
 evals = A_ss_red.eigenvals()
-print(evals.keys())
 q3d_check = 1/sqrt(3)
 print([evalf.N(i.subs({q3d: q3d_check})) for i in evals.keys()])
 
+zero = (f_0+f_1).col_join(f_2+f_3).col_join(f_a)
+rhs = solve(zero, qd+ud)
+rhs = Matrix([rhs[i].subs(eq_ud).subs(eq_u).subs(eq_qd).subs(qd_sym).subs(eq_q).subs(qd_sym_inv) for i in qd+ud])
+A_ss_bad_sym = rhs.jacobian(q+u)
+A_ss_bad_num = A_ss_bad_sym.subs({q1d: 0, q2: 0, m: 1, r: 1})
+nevals = A_ss_bad_num.eigenvals()
+print([evalf.N(i.subs({q3d: q3d_check})) for i in nevals.keys()])
